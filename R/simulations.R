a670eaa25e3e8d983bf1b04a4b1d849b43833203
Vincent Runge <vincent.runge@univ-evry.fr>
2020-03-23 10:49:38 +0100

    one.simu update

--- R/simulations.R
+++ R/simulations.R
@@ -1,11 +1,5 @@
 
-#library(parallel)
-#cores <- detectCores()
-#cores <- 40
 
-#############
-############# one.simu
-#############
 one.simu <- function(i, n = 10^5, phi = 0.5, sdEta2 = 1, sdNu2 = 1, nbK, poisParam = 0, meanGap = 2)
 {
   y <- dataARRW(n = n, poisParam, meanGap, phi = phi, sdEta2 = sdEta2, sdNu2 = sdNu2)
@@ -19,23 +13,3 @@ one.simu <- function(i, n = 10^5, phi = 0.5, sdEta2 = 1, sdNu2 = 1, nbK, poisPar
   return(df)
 }
 
-
-#########################################################################
-#nbSimu <- 100
-#df_test <- NULL
-#for(i in 1:10)
-#{
-#  lres_1 <- mclapply(1:nbSimu, FUN = one.simu,
-#                      n = 10^5,
-#                      phi = 0.1,
-#                     sdEta2 = 1,
-#                      sdNu2 = i,
-#                      nbK = 10,
-#                      mc.cores = cores)
-#df_test <- rbind(df_test, do.call(rbind, lres_1))
-#}
-#df_test
-
-
-
-#save(df_test, file="df_test.Rdata")
