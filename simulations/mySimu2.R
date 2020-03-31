d9f94a2d8ed5c1e584f68554164c9ef208fc2e6e
Vincent Runge <vincent.runge@univ-evry.fr>
2020-03-31 10:02:44 +0200

    new simu file

--- simulations/mySimu2.R
+++ simulations/mySimu2.R
@@ -1,71 +1,9 @@
 rm(list=ls())
 library(ARRWestim)
 library(parallel)
+library(fields)
 cores <- detectCores()
 cores <- 45
-
-#########################################################################
-#nbSimu <- 10
-#lres_1 <- mclapply(1:nbSimu, FUN = one.simu,
-#                   n = 10^5,
-#                   sdEta2 = 1,
-#                   sdNu2 = 1,
-#                   phi = 0.5,
-#                   poisParam = 0,
-#                   meanGap = 2,
-#                   nbK = 10,
-#                   mc.cores = cores)
-#df <- do.call(rbind, lres_1)
-#save(df, file="df.RData")
-##########################################################################
-
-nbSimu <- 50
-nbPhi <- 20
-nbOmega2 <- 20
-phi <- seq(from = 0, to = 1, length.out = nbPhi+1)
-phi <- phi[-(nbPhi+1)]
-omega2 <- seq(from = 0.5, to = 10, length.out = nbOmega2)
-res <- NULL
-for(i in phi)
-{
-  for(j in omega2)
-  {
-    res <- c(res, mclapply(1:nbSimu, FUN = one.simu,
-                       n = 10^5,
-                       sdEta2 = j,
-                       sdNu2 = 1,
-                       phi = i,
-                       poisParam = 0,
-                       meanGap = 2,
-                       nbK = 10,
-                       mc.cores = cores))
-    print(i)
-    print(j)
-  }
-}
-
-
-df <- do.call(rbind, res)
-save(df, file="df_sdEta2sdNu2Phi.RData")
-
-# analysis = some simple plots
-library(fields)
-dfmean <- aggregate(df,list(rep(1:(nrow(df)%/%nbSimu+1),each=nbSimu,len=nrow(df))),mean)[-1]
-z1 <- matrix(dfmean$`sdEta2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-image.plot(phi,omega2,z1)
-z2 <- matrix(dfmean$`sdNu2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-image.plot(phi,omega2,z2)
-z3 <- matrix(dfmean$phiEst, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-image.plot(phi,omega2,z3)
-
-dfsd <- aggregate(df,list(rep(1:(nrow(df)%/%nbSimu+1),each=nbSimu,len=nrow(df))),sd)[-1]
-w1 <- matrix(dfsd$`sdEta2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-image.plot(phi,omega2,w1)
-w2 <- matrix(dfsd$`sdNu2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-image.plot(phi,omega2,w2)
-w3 <- matrix(dfsd$phiEst, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-image.plot(phi,omega2,w3)
-
 
 ######################################################################
 # color scale function
@@ -102,51 +40,6 @@ colScale <- function(min, max, nb, epsilon)
   return(list(col=colorTable, breaks = brks))
 }
 
-####################
-# generating a 3x2 multi plot with correct color scales
-
-library(fields)
-
-dfmean <- aggregate(df,list(rep(1:(nrow(df)%/%nbSimu+1),each=nbSimu,len=nrow(df))),mean)[-1]
-z1 <- matrix(dfmean$`sdEta2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-z2 <- matrix(dfmean$`sdNu2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-z3 <- matrix(dfmean$phiEst, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-
-dfsd <- aggregate(df,list(rep(1:(nrow(df)%/%nbSimu+1),each=nbSimu,len=nrow(df))),sd)[-1]
-w1 <- matrix(dfsd$`sdEta2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-w2 <- matrix(dfsd$`sdNu2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-w3 <- matrix(dfsd$phiEst, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
-
-
-
-
-par(mfrow=c(3,2))
-par(mar = c(2,2,2,2))
-
-u1 <- colScale(min = min(z1), max = max(z1), epsilon = abs(max(z1))/2, nb = 20)
-image.plot(phi,omega2,z1,breaks=u1$breaks, col=u1$col ,axis.args=list(cex.axis=1.3),
-           main = expression(E((hat(sigma)[eta]-sigma[eta] )/ sigma[eta])))
-v1 <- colScale(min = 0, max = max(w1),  nb = 64)
-image.plot(phi,omega2,w1,breaks=v1$breaks, col=v1$col,axis.args=list(cex.axis=1.3),
-           main = expression(SD((hat(sigma)[eta]-sigma[eta] )/ sigma[eta])))
-
-u2 <- colScale(min = min(z2), max = max(z2), epsilon = abs(min(z2)/2), nb = 20)
-image.plot(phi,omega2,z2,breaks=u2$breaks, col=u2$col ,axis.args=list(cex.axis=1.3),
-           main = expression(E((hat(sigma)[nu]-sigma[nu] )/ sigma[nu])))
-v2 <- colScale(min = 0, max = max(w2),  nb = 64)
-image.plot(phi,omega2,w2,breaks=v2$breaks, col=v2$col,axis.args=list(cex.axis=1.3),
-           main = expression(SD((hat(sigma)[nu]-sigma[nu] )/ sigma[nu])))
-
-u3 <- colScale(min = min(z3), max = max(z3), epsilon = abs(max(z3)/2), nb = 20)
-image.plot(phi,omega2,z3,breaks=u3$breaks, col=u3$col ,axis.args=list(cex.axis=1.3),
-           main = expression(E (hat(phi))))
-v3 <- colScale(min = 0, max = max(w3),  nb = 64)
-image.plot(phi,omega2,w3,breaks=v3$breaks, col=v3$col,axis.args=list(cex.axis=1.3),
-           main = expression(SD (hat(phi))))
-
-
-########### ########### ########### ###########
-########### new test ########### ###########
 ########### ########### ########### ###########
 
 nbSimu <- 200
@@ -207,7 +100,6 @@ w2 <- matrix(dfsd$`sdNu2Est%`, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
 w3 <- matrix(dfsd$phiEst, nrow = nbPhi, ncol = nbOmega2, byrow = TRUE)
 
 
-library(fields)
 par(mfrow=c(3,2))
 par(mar = c(3,4,2,3))
 
@@ -269,5 +161,4 @@ image.plot(phi,logOmega2,w3,breaks=v3$breaks, col=v3$col,axis.args=list(cex.axis
            smallplot = c(0.85,0.89,0.13,0.91))
 axis(1, labels = c(0,0.2,0.4,0.6,0.8,0.95), at = c(0,0.2,0.4,0.6,0.8,0.95))
 axis(2, labels = myscale, at = positions)
-
 
