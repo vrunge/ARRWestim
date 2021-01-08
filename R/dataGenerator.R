1e8b8b0452298c362149c6bde4c01e3b8f200827
Vincent Runge <vincent.runge@univ-evry.fr>
2021-01-08 12:27:00 +0100

    Update dataGenerator.R

--- R/dataGenerator.R
+++ R/dataGenerator.R
@@ -1,28 +1,25 @@
 #' dataARRW
 #' @description Generate data with the ARRW model
-#' @param n number of data points
+#' @param model the initial model for change-point position and jump size
 #' @param sdEta2 variance in Random Walk
 #' @param sdNu2 variance in AR(1)
 #' @param phi AR(1) autocorrelation parameter
-#' @param poisParam a probability in Poisson distribution for changepoint density
-#' @param meanGap mean gap of the jumps
 #' @return a vector of simulated data with y = the data, mu = the signal, changepoints = changepoint indices
 #' @examples
-#' myData <- dataARRW(100)
-dataARRW <- function(n = 1e3, sdEta2 = 1, sdNu2 = 1, phi = 0.9, poisParam = 0, meanGap = 2)
+#' myData <- dataARRW(scenarioGenerator(100, "rand1", nbSeg = 20))
+dataARRW <- function(model, sdEta2 = 1, sdNu2 = 1, phi = 0.9)
 {
-  changepoints <- rpois(n, poisParam)
-  changes <- cumsum(sample(c(-1, 1), size = n, replace = TRUE) * changepoints * rnorm(n, mean = meanGap))
+  n <- length(model)
   RW <- cumsum(rnorm(n, 0, sqrt(sdEta2)))
-  res <- changes + RW
 
+  y <- model + RW
   e <- rnorm(1, 0, sqrt(sdNu2)/sqrt(1 - phi^2))
   for(i in 1:n)
   {
-    res[i] <- res[i] + e
+    y[i] <- y[i] + e
     e <- phi*e + rnorm(n = 1, mean = 0, sd = sqrt(sdNu2))
   }
-  return(list(y = res, mu = changes + RW, changepoints = which(changepoints > 0)))
+  return(list(y = y, mu = model + RW, changepoints = which(diff(model) != 0)))
 }
 
 
@@ -75,3 +72,4 @@ scenarioGenerator <- function(N, type = c("none", "up", "updown", "rand1"), nbSe
     rand1 = unlist(sapply(1:nbSeg, function(i) rep(rand1Jump[i] * jumpSize, r1[i])))
   )
 }
+
